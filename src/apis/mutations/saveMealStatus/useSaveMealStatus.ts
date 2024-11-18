8c3a3a26ad694ad68092b4a4b2eb15691b481bf4
Siddhu31124 <kunurusiddhartha@gmail.com>
2024-11-18 16:16:51 +0530

    #

--- src/apis/mutations/saveMealStatus/useSaveMealStatus.ts
+++ src/apis/mutations/saveMealStatus/useSaveMealStatus.ts
@@ -5,10 +5,7 @@ import { MealStatusEnum } from "../../../types";
 function useSaveMealStatus() {
   const [saveMealStatus, { data, loading, error }] =
     useMutation(SAVE_MEAL_STATUS);
-  function triggerSaveMealStatue(data: {
-    mealId: string;
-    status: MealStatusEnum;
-  }) {
+  function triggerSaveMealStatue(data: { status: MealStatusEnum }) {
     saveMealStatus({ variables: { ...data } });
   }
   return { triggerSaveMealStatue, loading, error };
