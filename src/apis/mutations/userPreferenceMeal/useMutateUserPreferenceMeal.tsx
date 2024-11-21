c98a806ca33ece59974c19d16a83d23b5ca682b9
Siddhu31124 <kunurusiddhartha@gmail.com>
2024-11-21 18:49:48 +0530

    added i18n

--- src/apis/mutations/userPreferenceMeal/useMutateUserPreferenceMeal.tsx
+++ src/apis/mutations/userPreferenceMeal/useMutateUserPreferenceMeal.tsx
@@ -1,12 +1,26 @@
 import { useMutation } from "@apollo/client";
 import USER_PREFERENCE_MEAL from "./mutation";
+import { successToast } from "../../../utils/toastUtils/successToast";
+import { failureToast } from "../../../utils/toastUtils/failureToast";
+import { VoidFunctionType } from "../../../types";
+import ModalStore from "../../../store/ModalStore";
 
-function useMutateUserPreference() {
-  const [userPreference, { data, loading, error }] =
-    useMutation(USER_PREFERENCE_MEAL);
+function useMutateUserPreference(mealType: string, action: VoidFunctionType) {
+  const [userPreference, { data, loading, error }] = useMutation(
+    USER_PREFERENCE_MEAL,
+    {
+      onCompleted: handelSuccussAdd,
+      onError: () => failureToast("Failed to Add Try Again"),
+    }
+  );
 
-  function triggerUserPreference() {
-    userPreference;
+  function triggerUserPreference(data: any) {
+    userPreference({ variables: { params: { ...data } } });
+  }
+  function handelSuccussAdd() {
+    successToast("Meal Add Successfully");
+    action();
+    ModalStore.closeModal();
   }
   return { triggerUserPreference, loading, error };
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,26 +5,31 @@ import {
   InMemoryCache,
   HttpLink,
 } from "@apollo/client";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n.ts";
 
 import "./index.css";
 import App from "./App.tsx";
-import { ACCESS_TOKEN, GRAPHQL_END_POINT } from "./constants.ts";
+import { ACCESS_TOKEN, GRAPHQL_URL } from "./constants.ts";
+
 let accessToken = localStorage.getItem(ACCESS_TOKEN);
 if (accessToken) {
   accessToken = JSON.parse(accessToken);
 }
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: `${GRAPHQL_END_POINT}/graphql`,
+    uri: GRAPHQL_URL,
     headers: {
-      Authorization: `Bearer c91e654a-009e-43fd-ba25-d7edf5d546ca`,
+      Authorization: `Bearer ${accessToken}`,
     },
   }),
   cache: new InMemoryCache(),
 });
 
 createRoot(document.getElementById("root")!).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <I18nextProvider i18n={i18n}>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </I18nextProvider>
 );
